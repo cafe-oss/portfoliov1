1c61405788ebedcad7da9ebf580d97626cf55cd3
Jhon Cafe <cafe.jhonalvin12@gmail.com>
2025-05-07 13:59:12 +0800

    updated portfolio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function ($) {});
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(SplitText);
 
   ScrollTrigger.matchMedia({
     "(min-width: 769px)": function () {
@@ -38,34 +39,47 @@ document.addEventListener("DOMContentLoaded", (event) => {
         });
       }
 
-      const ja_h1 = $(".ja-sidebar-header > h1");
+      let ja_h1 = SplitText.create(".ja-sidebar-header > h1", {
+        type: "chars",
+      });
       if (ja_h1.length != 0) {
-        gsap.from(ja_h1, {
-          opacity: 0,
-          x: "-100%",
-          duration: 1,
-          ease: "power2.out",
+        gsap.from(ja_h1.chars, {
+          y: 20,
+          autoAlpha: 0,
+          stagger: 0.03,
         });
       }
 
-      const ja_h2 = $(".ja-sidebar-header > h2");
+      let ja_h2 = SplitText.create(".ja-sidebar-header > h2", {
+        type: "chars",
+      });
       if (ja_h2.length != 0) {
-        gsap.from(ja_h2, {
-          opacity: 0,
-          x: "-100%",
-          duration: 1,
+        gsap.from(ja_h2.chars, {
+          y: 20,
           delay: 0.1,
-          ease: "power2.out",
+          autoAlpha: 0,
+          stagger: 0.03,
         });
       }
 
-      const ja_p = $(".ja-sidebar-header > p");
+      let ja_p = SplitText.create(".ja-sidebar-header > p", {
+        type: "chars",
+      });
       if (ja_p.length != 0) {
-        gsap.from(ja_p, {
+        gsap.from(ja_p.chars, {
+          y: 20,
+          delay: 0.1,
+          autoAlpha: 0,
+          stagger: 0.03,
+        });
+      }
+
+      const ja_social_links = $(".ja-sidebar-social-links > ul");
+      if (ja_social_links != 0) {
+        gsap.from(ja_social_links, {
           opacity: 0,
-          x: "-100%",
+          y: "100%",
           duration: 1,
-          delay: 0.5,
           ease: "power2.out",
         });
       }
@@ -80,11 +94,45 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
         gsap.to(ja_img, {
           opacity: 1,
-          duration: 1,
+          duration: 2,
           delay: 0.1,
           ease: "power3.out",
         });
       }
+
+      const cards_wrappers = gsap.utils.toArray(
+        ".ja-experience .ja-card-wrapper"
+      );
+      const cards = gsap.utils.toArray(".ja-experience .ja-card-wrapper > div");
+
+      cards_wrappers.forEach((wrapper, i) => {
+        const card = cards[i];
+        let scale = 1,
+          rotation = 0;
+        if (i !== cards.length - 1) {
+          scale = 0.9 + 0.025 * i;
+          rotation = -10;
+        }
+        gsap.to(card, {
+          scale: scale,
+          rotationX: rotation,
+          transformOrigin: "top center",
+          ease: "none",
+          scrollTrigger: {
+            trigger: wrapper,
+            start: "top " + (200 + 40 * i),
+            end: "bottom 550",
+            endTrigger: ".ja-card-container",
+            scrub: true,
+            pin: wrapper,
+            pinSpacing: false,
+            id: i + 1,
+
+            onEnter: () => wrapper.classList.add("active-bg"),
+            onLeaveBack: () => wrapper.classList.remove("active-bg"),
+          },
+        });
+      });
     },
   });
 });
